2e5f710a427a9bc81bc6f6ea48680841684c6856
yassinetems15-dot <yassine.tems15@gmail.com>
2026-04-29 00:08:20 +0100

    Add files via upload

---
+++ src/Cine/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GestiondunCinema {
+    requires java.sql;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    exports Cine;
+}
